4dec1df79b953fde73fe136810d915e0a8999fd1
Ashley Andrade <ashleyvitoriaandrade2004@gmail.com>
2026-06-15 19:07:01 -0300

    Atualizações do js

---
+++ js/script_repeticao.js
@@ -0,0 +1,28 @@
+//PEGANDO ELEMENTOS DO DOM
+const divFor = document.querySelector('#div-FormData')
+
+//ESTRUTURAS DE REPETIÇÕES
+// TODA E QUALQUER ESTRUTURA DE REPETIÇÃO, OBRIGATORIAMENTE TEM QUE TER UMA CONDIÇÃO DE PARADA
+
+//ESTRUTURAS DE REPETIÇÕES
+for (i = 0; i < 10; i++){
+    console.log(i)
+    divFor.innerHTML += `${i} <br>`
+}
+
+}
+
+//CONTADOR E ACUMULADOR
+const inputNum1 = document.querySelector('#num1')
+const btnNum1 = document.querySelector('#btn-num1')
+const divResult = document.querySelector('#result-cont-acum')
+let cont = 0
+let acum = 0.0
+btnNum1.addEventListener('click',(evt)=> {
+    let numDigitado = Number (inputNum1.value)
+
+    cont++
+    cont += numDigitado
+
+    divResult.innerHTML = `Total de número digitado ${cont} <br> A soma dos números digitados ${acum}`
+})
