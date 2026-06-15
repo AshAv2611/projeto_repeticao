25ecd31f2be1f5c14da01dca6f6eb904e7a44a26
Ashley Andrade <ashleyvitoriaandrade2004@gmail.com>
2026-06-15 19:13:54 -0300

    Alterações adicionadas

--- js/script_repeticao.js
+++ js/script_repeticao.js
@@ -1,5 +1,5 @@
 //PEGANDO ELEMENTOS DO DOM
-const divFor = document.querySelector('#div-FormData')
+const divFor = document.querySelector('#div-for')
 
 //ESTRUTURAS DE REPETIÇÕES
 // TODA E QUALQUER ESTRUTURA DE REPETIÇÃO, OBRIGATORIAMENTE TEM QUE TER UMA CONDIÇÃO DE PARADA
@@ -10,7 +10,6 @@ for (i = 0; i < 10; i++){
     divFor.innerHTML += `${i} <br>`
 }
 
-}
 
 //CONTADOR E ACUMULADOR
 const inputNum1 = document.querySelector('#num1')
@@ -22,7 +21,7 @@ btnNum1.addEventListener('click',(evt)=> {
     let numDigitado = Number (inputNum1.value)
 
     cont++
-    cont += numDigitado
+    acum += numDigitado
 
     divResult.innerHTML = `Total de número digitado ${cont} <br> A soma dos números digitados ${acum}`
 })
